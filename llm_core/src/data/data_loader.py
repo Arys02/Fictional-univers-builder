1d6af1c6099d59296ab36a7d603ede0b22659597
charles yaiche <charles.yaiche@epita.fr>
2025-06-17 13:46:42 +0200

    implement data loader

--- llm_core/src/data/data_loader.py
+++ llm_core/src/data/data_loader.py
@@ -1,3 +1,26 @@
+from pathlib import Path
 
-# class DataLoader:
-#     def __init__(self, data_path=None):
+from llm_core.config import TOKENIZED_DATA_DIR, RAW_DATA_DIR
+
+
+class DataLoader:
+    def __init__(self, path: Path, type: str, device):
+        if type == 'tokens':
+            self.data = torch.load(path).to(device)
+        elif type == 'raw':
+            with open(path, 'r', encoding='utf-8') as f:
+                self.rawdata = f.read()
+
+    @classmethod
+    def fromTokens(cls, type: str, dataset: str, vocab_size: int, device):
+        return cls(TOKENIZED_DATA_DIR / f'tokenizer_{type}_{dataset}_{vocab_size}.pt', 'tokens', device)
+
+    @classmethod
+    def fromRaw(cls, dataset: str):
+        return cls(RAW_DATA_DIR / f'{dataset}.txt', 'raw', "")
+
+    def split(self, val_size):
+        n = int(len(self.data) * val_size)
+        train_data = self.data[:n]
+        val_data = self.data[n:]
+        return train_data, val_data
