7cfcd311eb6626dc3e2864dc7016483346255f18
clementdevarieux <devarieux.clement@gmail.com>
2025-07-13 17:01:24 +0200

    feat: cleaning work dir

--- poc/dropdb.py
+++
@@ -1,9 +0,0 @@
-import os
-
-db_path = '../database.db'
-
-if os.path.exists(db_path):
-    os.remove(db_path)
-    print(f"{db_path} has been deleted successfully!")
-else:
-    print(f"{db_path} does not exist.")
